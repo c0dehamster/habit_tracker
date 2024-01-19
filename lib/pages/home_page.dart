f298bac27772fbf765cd7d3b2c18e43c7c913dae
Sergej Smelov <smelowsergey@gmail.com>
2024-01-19 15:56:16 +0500

    Add UI for adding a new habit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/main_drawer.dart';
+import 'package:habit_tracker/models/database/habit_database.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,11 +11,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Text controller
+  final TextEditingController textController = TextEditingController();
+
+  // Create a new habit
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(hintText: "Create a new habit"),
+        ),
+        actions: [
+          // Save button
+          TextButton(
+            onPressed: () {
+              // Get the new habit name
+              String newHabitName = textController.text;
+
+              // Save to the database
+              context.read<HabitDatabase>().addHabit(newHabitName);
+
+              // Pop the dialog box
+              Navigator.pop(context);
+
+              // Clear the controller
+              textController.clear();
+            },
+            child: const Text("Save"),
+          ),
+
+          // Cancel button
+          TextButton(
+            onPressed: () {
+              // Pop the dialog box
+              Navigator.pop(context);
+
+              // Clear the controller
+              textController.clear();
+            },
+            child: const Text("Cancel"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(),
       drawer: const MainDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.edit,
+        ),
+      ),
     );
   }
 }
